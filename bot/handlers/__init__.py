0877f9cc9b88c8b98e66568be08762f72ab78f43
hhh <shshshsh@horsefucker.org>
2024-07-21 16:43:03 +0300

    removed like middleware

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,11 +1,7 @@
 from aiogram import Router
 
-from bot.middlewares import LikeMiddleware
-
 from . import initialize, start, tiktok
 
 router = Router()
-
-router.message.middleware(LikeMiddleware())
 
 router.include_routers(initialize.router, start.router, tiktok.router)
